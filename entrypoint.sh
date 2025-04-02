5b78458584d52ef6e3e72fb64c162a5ec2dbcc1a
Nikolas "Blackout" Lucansky <51444462+nlucansk@users.noreply.github.com>
2025-04-02 22:05:24 +0200

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,26 +8,28 @@ REPO_TOKEN=$4
 #TARGET_URL=$5
 
 # Run Horusec scan
-horusec start --project-path . -D
-horusec start --project-path . --output-format json --json-output-file horusec-report.json -D
+# horusec start --project-path . -D
+# horusec start --project-path . --output-format json --json-output-file horusec-report.json -D
 
 # Run Dependency-Check scan
+ls -la ./dependency-check/
+ls -la ./dependency-check/bin
 ./dependency-check/bin/dependency-check.sh --project "MyProject" --scan "./" --format "XML" --out "dependency-check-report.xml"
 
 # # Run OWASP ZAP scan
 # docker run --rm -v $(pwd):/zap/wrk/ -t owasp/zap2docker-stable zap-baseline.py -t $TARGET_URL -r zap_report.xml
 
 # Run Dastardly scan
-docker run --rm -v $(pwd):/src dastardly-ci/dastardly scan --project . --output dastardly-report.xml
+#docker run --rm -v $(pwd):/src dastardly-ci/dastardly scan --project . --output dastardly-report.xml
 
-# Upload results to DefectDojo
-curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
-     -H "Authorization: Token $DEFECTDOJO_API_KEY" \
-     -F 'scan_type=Horusec Scan' \
-     -F "engagement=$ENGAGEMENT_ID" \
-     -F 'file=@horusec-report.json' \
-     -F 'active=true' \
-     -F 'verified=true'
+# # Upload results to DefectDojo
+# curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
+#      -H "Authorization: Token $DEFECTDOJO_API_KEY" \
+#      -F 'scan_type=Horusec Scan' \
+#      -F "engagement=$ENGAGEMENT_ID" \
+#      -F 'file=@horusec-report.json' \
+#      -F 'active=true' \
+#      -F 'verified=true'
 
 curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
      -H "Authorization: Token $DEFECTDOJO_API_KEY" \
@@ -37,18 +39,18 @@ curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
      -F 'active=true' \
      -F 'verified=true'
 
-curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
-     -H "Authorization: Token $DEFECTDOJO_API_KEY" \
-     -F 'scan_type=ZAP Scan' \
-     -F "engagement=$ENGAGEMENT_ID" \
-     -F 'file=@zap_report.xml' \
-     -F 'active=true' \
-     -F 'verified=true'
+# curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
+#      -H "Authorization: Token $DEFECTDOJO_API_KEY" \
+#      -F 'scan_type=ZAP Scan' \
+#      -F "engagement=$ENGAGEMENT_ID" \
+#      -F 'file=@zap_report.xml' \
+#      -F 'active=true' \
+#      -F 'verified=true'
 
-curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
-     -H "Authorization: Token $DEFECTDOJO_API_KEY" \
-     -F 'scan_type=Burp Dastardly Scan' \
-     -F "engagement=$ENGAGEMENT_ID" \
-     -F 'file=@dastardly-report.xml' \
-     -F 'active=true' \
-     -F 'verified=true'
+# curl -X POST "$DEFECTDOJO_URL/api/v2/import-scan/" \
+#      -H "Authorization: Token $DEFECTDOJO_API_KEY" \
+#      -F 'scan_type=Burp Dastardly Scan' \
+#      -F "engagement=$ENGAGEMENT_ID" \
+#      -F 'file=@dastardly-report.xml' \
+#      -F 'active=true' \
+#      -F 'verified=true'
